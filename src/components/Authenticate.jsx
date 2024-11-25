178f198b74ed38e139a63d90dce2d4b339b85239
locedu <locutus.edu@outlook.com>
2024-11-25 00:47:50 -0500

    add: preventDefault on clickHandler

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -6,10 +6,14 @@ export default function Authenticate({ token }) {
   const [error, setError] = useState(null);
   // state: success message
   const [successMessage, setSuccessMessage] = useState();
+
+  function handleClick(e){
+    e.preventDefault();
+    authenticate();
+  }
   // async method, calling API with token..
   const authenticate = async () => {
     console.log("authenticate", token);
-
     try {
       const url = "https://fsa-jwt-practice.herokuapp.com/authenticate";
       const headers = {
@@ -20,7 +24,8 @@ export default function Authenticate({ token }) {
       const response = await fetch(url, params);
       const result = await response.json();
       console.log("result", result);
-      setSuccessMessage(result.message);
+      const msg = result.data.username ? `${result.message}. Thank you, ${result.data.username}`: result.message;
+      setSuccessMessage(msg);
     } catch (error) {
       console.log("oopsy!");
       setError(error.message);
@@ -28,13 +33,19 @@ export default function Authenticate({ token }) {
   };
   return (
     <div>
-      <h2 style={!token ? {color: "lightgrey"}: {color: "black"} }>Authenticate</h2>
+      <h2 style={!token ? { color: "lightgrey" } : { color: "black" }}>
+        Authenticate
+      </h2>
       {/* success message from API call.. */}
       {successMessage && <p>{successMessage}</p>}
       {/* try-catch error */}
       {error && <p>{error}</p>}
       {/* authenticate button here.. */}
-      <button className="btn btn-primary" onClick={authenticate} disabled={!token}>
+      <button
+        className="btn btn-primary"
+        onClick={handleClick}
+        disabled={!token}
+      >
         Authenticate
       </button>
     </div>
